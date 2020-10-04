6257ce135857a9f57aa97e849409bdc122c75b03
yanirbuznah <65356872+yanirbuznah@users.noreply.github.com>
2020-10-04 15:40:46 +0300

    about us page

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
-const _ = require("lodash");
+//const _ = require("lodash");
 const mongoose = require("mongoose");
 
 const homeStartingContent = 'דתיים במדמ"ח - חומר לשבת, היא קבוצה שנוצרה על מנת שסטודנטים יוכלו לשתף את חבריהם בחומרים להדפסה נוחה כך שיוכלו לקרוא בשבת. סיכומים, חומרי העשרה, הקדמות ועוד.. <br> מהר מאוד התחילו גם דברי תורה המשלבים את פרשת השבוע והמקצוע שבחרנו ללמוד.. מכיוון שהושקעה הרבה מחשבה בדברי התורה וההקשרים למדעי המחשב לא פעם הם לא פחות ממדהימים. החלטנו להעלות אותם לאינטרנט.  <br> תהנו!'
 //const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const contactContent = "מוזמנים להצטרף לקבוצת הוואטסאפ או להתרשם, להדפיס ולהוסיף חומרים חדשים לדרייב!";
-//const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const aboutContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
+const joinContent = "מוזמנים להצטרף לקבוצת הוואטסאפ או להתרשם, להדפיס ולהוסיף חומרים חדשים לדרייב!";
+//const joinContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
+const aboutContent =  'דתיים במדמ"ח - חומר לשבת, היא קבוצה שנוצרה על מנת שסטודנטים יוכלו לשתף את חבריהם בחומרים להדפסה נוחה כך שיוכלו לקרוא בשבת. סיכומים, חומרי העשרה, הקדמות ועוד.. <br> הקבוצה נוצרה על ידי סטודנטים בשנה א של מדעי המחשב בבר אילן, עם זאת, אפשר להצטרף לקבוצה גם אם אינכם סטודנטים בבר אילן או בכלל.  את הלינקים לקבוצת הוואטסאם והדרייב תוכלו למצוא בלשונית JOIN US. <br> מהר מאוד התחילו גם דברי תורה המשלבים את פרשת השבוע והמקצוע שבחרנו ללמוד.. מכיוון שהושקעה הרבה מחשבה בדברי התורה וההקשרים למדעי המחשב לא פעם הם לא פחות ממדהימים. החלטנו להעלות אותם לאינטרנט.  <br> ';
+  //const aboutContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 
 //mongodb://localhost:27017/blogDB
@@ -25,7 +26,6 @@ const postSchema = {
 };
 const Post = mongoose.model("Post", postSchema);
 
-let posts = [];
 
 const app = express();
 
@@ -52,9 +52,9 @@ app.get('/about', function(req, res) {
   });
 });
 
-app.get('/contact', function(req, res) {
-  res.render('contact', {
-    contactContent: contactContent
+app.get('/join', function(req, res) {
+  res.render('join', {
+    joinContent: joinContent
   });
 });
 
@@ -94,7 +94,7 @@ app.get('*', function(req, res) {
 
 
 let port = process.env.PORT;
-if (port == null || port == "") {
+if (port == null || port === "") {
   port = 3000;
 }
 
